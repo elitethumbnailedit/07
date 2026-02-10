8e489443e2178b85126598dca9104f1348c815b3
elitethumbnailedit <neststory9142@gmail.com>
2026-02-10 18:51:42 +0530

    Update script.js
    Signed-off-by: elitethumbnailedit <neststory9142@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function toggleSidebar() {
-  document.getElementById("sidebar").classList.toggle("active");
-}
+
+  const menuBtn = document.querySelector('.menu-btn');
+  const sidebar = document.querySelector('.sidebar');
+  const overlay = document.getElementById('overlay');
+
+  menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+
+  overlay.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+  });
+
